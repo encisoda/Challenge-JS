10ed9b11d3146c4ad3d487aea4b129001ee64b7d
Daniel Enciso <encisoda@gmail.com>
2020-10-06 02:57:34 -0300

    Se agrego configuraciones en el servidor

--- server/build/index.js
+++ server/build/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
 const indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
 const operationsRoutes_1 = __importDefault(require("./routes/operationsRoutes"));
 class Server {
@@ -16,10 +17,13 @@ class Server {
     config() {
         this.app.set('port', process.env.PORT || 3000);
         this.app.use(morgan_1.default('dev'));
+        this.app.use(cors_1.default());
+        this.app.use(express_1.default.json());
+        this.app.use(express_1.default.urlencoded({ extended: false }));
     }
     routes() {
         this.app.use(indexRoutes_1.default);
-        this.app.use('/operations', operationsRoutes_1.default);
+        this.app.use('/api/operations', operationsRoutes_1.default);
     }
     start() {
         this.app.listen(this.app.get('port'), () => {
